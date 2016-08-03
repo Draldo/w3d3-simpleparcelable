767166549379bb3aaa31362d7a05defa43ec03a1
Draldo <draldo_lopez@hotmail.com>
2016-08-03 16:30:47 -0400

    Get Json

--- app/src/main/java/com/example/admin/simpleparcelable/MainActivity.java
+++ app/src/main/java/com/example/admin/simpleparcelable/MainActivity.java
@@ -7,10 +7,15 @@ import android.view.View;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String jsonComplex = "{\"students\":[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]}";
+    private String jsonString = "{\"students\":[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Gson gson = new GsonBuilder().create();
-        Result result = gson.fromJson(jsonComplex, Result.class);
+        Type listType = new TypeToken<List<Student>>() {}.getType();
+
+        ArrayList<Student> students = gson.fromJson(jsonString, listType);
 
     }
 
