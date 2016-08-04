bd99e551975a38d306174f902631f63779c24e99
Draldo <draldo_lopez@hotmail.com>
2016-08-04 00:28:45 -0400

    Changing orientation withouth fragments

--- app/src/main/java/com/example/admin/simpleparcelable/MainActivity.java
+++ app/src/main/java/com/example/admin/simpleparcelable/MainActivity.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -26,10 +28,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainTAG_";
     private String jsonString = "[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]";
     private EditText mEditText;
+    private ImageView mImageview;
     private ArrayList<Student> students;
     private ArrayList<String> array;
     private ListView mListview;
     private ArrayAdapter<String> adapter;
+    private static final String[] img = new String[]{"http://goo.gl/Jy4clk","http://goo.gl/7Wa3Tv","http://goo.gl/AzrjpX","http://goo.gl/HlF9o7","https://goo.gl/BmIp1G"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +55,20 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
         mListview.setAdapter(adapter);
 
+        mImageview = (ImageView) findViewById(R.id.imageview_item);
+
         mListview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String[] str = adapter.getItem(position).split(" ");
                 Toast.makeText(view.getContext(), str[0], Toast.LENGTH_SHORT).show();
+                if(mImageview!=null) {
+                    Picasso
+                            .with(view.getContext())
+                            .load(img[position])
+                            .fit()
+                            .into(mImageview);
+                }
             }
         });
 
